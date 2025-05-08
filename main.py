5d1cd48d27d1f14f7a7d746fe17e791f2fe63157
Ashna Guha <57196878+TheLabradorScientist@users.noreply.github.com>
2025-05-08 12:50:44 -0400

    commit fixed equation

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import numpy as np
+import math
 import matplotlib.pyplot as plt
 
 # EQUATION CREDIT: https://journals.asm.org/doi/10.1128/aem.71.12.7920-7926.2005
-# " dN/dt = rN {1 -(N/Nmax)m}{1 -(Nmin/N)n} (equation 3) 
+# " dN/dt = rN {1 -(N/Nmax)^m}{1 -(Nmin/N)^n} (equation 3) 
 # Here, we will denote equation 3 as model III. Model III can describe 
 # sigmoidal curves with various curvatures during the deceleration phase 
 # compared to model II. With a larger m, the curvature of the deceleration 
@@ -11,9 +12,9 @@ import matplotlib.pyplot as plt
 
 # Hyperparameters
 duration = 24 # (hours)
-logN0 = 3.8 # Initial population (log)
-logN_max = 10.1 # Max population (log)
-logN_min = 0 # Min population (log)
+N0 = 10**3.8 # Initial population
+N_max = 10**10.1 # Max population
+N_min = 10**1 # Min population
 
 milk_adj = 1/6 # Milk adjustment factor (20 min in LB / 120 min in skim milk) doubling time
 
@@ -27,37 +28,37 @@ FR_n = 3.3 # Adjustment factor (FR)
 
 # Plot data
 time = [] # Time (hours)
-LB_rt = [] # CFU (room temp in LB)
-rt = [] # CFU (room temp in MILK)
-hs = [] # CFU (heat shock in MILK)
-fr = [] # CFU (refrigeration in MILK)
+LB_rt = [] # log CFU (room temp in LB)
+rt = [] # log CFU (room temp in MILK)
+hs = [] # log CFU (heat shock in MILK)
+fr = [] # log CFU (refrigeration in MILK)
 
 # Setup initial CFUs
-LB_rt_cfu = logN0
-rt_cfu = logN0
-hs_cfu = logN0 * 0.2
-fr_cfu = logN0
+LB_rt_cfu = N0
+rt_cfu = N0
+hs_cfu = N0 * 0.2
+fr_cfu = N0
 
 # Loop over duration
 for i in range(duration):
-    # Append most recent values
-    LB_rt.append(LB_rt_cfu)
-    rt.append(rt_cfu)
-    hs.append(hs_cfu)
-    fr.append(fr_cfu)
+    # Append log of most recent values
+    LB_rt.append(math.log10(LB_rt_cfu))
+    rt.append(math.log10(rt_cfu))
+    hs.append(math.log10(hs_cfu))
+    fr.append(math.log10(fr_cfu))
     time.append(i)
 
     # Obtain most recent CFU values
-    LB_RT_N = LB_rt[-1]
-    RT_N = rt[-1]
-    HS_N = hs[-1]
-    FR_N = fr[-1]
+    LB_RT_N = LB_rt_cfu
+    RT_N = rt_cfu
+    HS_N = hs_cfu
+    FR_N = fr_cfu
 
     # Obtain new rate differentials
-    LB_RT_dn_dt = RT_r * LB_RT_N * (1 - (RT_m * LB_RT_N / logN_max) * (1 - (RT_n * logN_min / LB_RT_N)))
-    RT_dn_dt = (RT_r * milk_adj) * RT_N * (1 - (RT_m * RT_N / logN_max) * (1 - (RT_n * logN_min / RT_N)))
-    HS_dn_dt = (RT_r * milk_adj) * HS_N * (1 - (RT_m * HS_N / logN_max) * (1 - (RT_n * logN_min / HS_N)))
-    FR_dn_dt = (FR_r * milk_adj) * FR_N * (1 - (FR_m * FR_N / logN_max) * (1 - (FR_n * logN_min / FR_N)))
+    LB_RT_dn_dt = RT_r * LB_RT_N * (1 - (LB_RT_N / N_max)**RT_m) * (1 - (N_min / LB_RT_N)**RT_n)
+    RT_dn_dt = (RT_r * milk_adj) * RT_N * (1 - (RT_N / N_max)**RT_m) * (1 - (N_min / RT_N)**RT_n)
+    HS_dn_dt = (RT_r * milk_adj) * HS_N * (1 - (HS_N / N_max)**RT_m) * (1 - (N_min / HS_N)**RT_n)
+    FR_dn_dt = (FR_r * milk_adj) * FR_N * (1 - (FR_N / N_max)**FR_m) * (1 - (N_min / FR_N)**FR_n)
 
     # Calculate new CFUs
     LB_rt_cfu += LB_RT_dn_dt
